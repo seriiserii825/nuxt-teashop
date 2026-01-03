466d32bd4a07f651583d869d701c85da00db02a8
serii <seriiburduja@gmail.com>
2026-01-03 10:28:16 +0200

    feat: home page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,9 +2,8 @@
 
 <template>
   <div class="roboto min-h-screen bg-slate-900 text-white">
-    <AppHeader class="z-10 relative" />
     <div class="container">
-      <slot ></slot>
+      <slot></slot>
     </div>
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, error
+    alias expedita numquam ea quod harum provident saepe ipsam atque aspernatur
+    possimus eaque, fuga iste, minus incidunt tempora consequuntur eveniet.
+  </div>
+</template>
