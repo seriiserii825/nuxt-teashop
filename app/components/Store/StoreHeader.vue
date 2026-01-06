12794ddf7105d3e57bb2f56e9f6cf906dd91f29e
serii <seriiburduja@gmail.com>
2026-01-06 23:38:18 +0200

    feat: store header

--- app/components/Store/StoreHeader.vue
+++ app/components/Store/StoreHeader.vue
@@ -4,6 +4,10 @@
   <div
     class="flex w-full items-center gap-x-4 border-b bg-white px-4 py-4 shadow-sm"
   >
+    <font-awesome-icon
+      :icon="['fas', 'bars']"
+      class="hidden text-xl lg:block"
+    />
     <nav class="mr-auto flex items-center"></nav>
     <div class="flex items-center gap-x-4">
       <StoreSwitcher />
