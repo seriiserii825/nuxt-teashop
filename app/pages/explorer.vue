425debbd4ec561029e54417f87ed69a95431f323
serii burduja <seriiburduja@gmail.com>
2026-01-27 11:02:27 +0200

    feat: search query for explorer page

---
+++ app/pages/explorer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const route = useRoute()
+  const search_query = computed(() => (route.query.q as string) || '')
+</script>
+
+<template>
+  <section class="bg-white py-16">
+    <div v-if="search_query" class="container mx-auto px-4">
+      <FrontHeaders :title="`Search for '${search_query}'`" />
+      <ProductGrid />
+    </div>
+    <div v-else class="container mx-auto px-4">
+      <h2 class="mb-4 text-2xl font-bold">
+        Please enter a search query, for example: shoes
+      </h2>
+    </div>
+  </section>
+</template>
