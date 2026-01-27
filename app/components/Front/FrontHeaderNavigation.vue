3c997c04004929919dbb4412db68fc26b92b0a1f
serii burduja <seriiburduja@gmail.com>
2026-01-27 12:43:49 +0200

    feat: favorites page

--- app/components/Front/FrontHeaderNavigation.vue
+++ app/components/Front/FrontHeaderNavigation.vue
@@ -6,6 +6,11 @@
     <NuxtLink to="/catalog" class="text-gray-700 transition hover:text-blue-600"
       >Catalog</NuxtLink
     >
+    <NuxtLink
+      to="/favorites"
+      class="text-gray-700 transition hover:text-blue-600"
+      >Favorites</NuxtLink
+    >
     <button
       class="flex items-center space-x-2 rounded-lg bg-blue-600 px-6 py-2 text-white transition hover:bg-blue-700"
     >
---
+++ app/pages/favorites.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="bg-white py-16">
+    <div class="container mx-auto px-4">
+      <FrontHeaders
+        title="Favorites"
+        text="Our customers' favorite products."
+      />
+      <ProductGrid />
+    </div>
+  </section>
+</template>
